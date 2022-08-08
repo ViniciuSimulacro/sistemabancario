68468a2ba484660bbbd97c7157cba92d70615632
ViniciusSimulacro <delucca86@gmail.com>
2022-08-07 21:42:54 -0300

    dividido em duas classes cliente e ação_cliente

--- sistema.py
+++ sistema.py
@@ -1,8 +1,33 @@
+from random import randint
+
 class cliente:
-    def __init__(self,nome,conta,saldo) -> None:
+    def __init__(self, nome, conta, saldo = 0 ) -> None:
         self.nome = nome
+        self.documento = ()
         self.conta = conta
         self.saldo = saldo
+
+    def cadastra_cliente(self):
+        self.nome = str(input('Qual seu nome completo: '))
+        self.documento = str(input('Qual o numero do seu documento: '))
+        self.conta = randint(00000,99999)
+        self.digito = randint(0,9)
+        self.saldo = 0
+        print(f'{self.nome}, o numero da sua conta aberta é {self.conta}-{self.digito}')
+
+    def lista_cliente(self):
+        lista_clientes = []
+        cliente_conta = {'nome': self.nome, 'documento': self.documento, 'conta': (f'{self.conta}-{self.digito}'), 'saldo': self.saldo}
+        lista_clientes.append(cliente_conta)
+        print(lista_clientes)
+    
+    
+        
+
+
+class acao_cliente(cliente):    
+    def __init__(self, nome, conta, saldo=0) -> None:
+        super().__init__(nome, conta, saldo)
 
     def consulta_saldo(self):
         print(f'O saldo do cliente é {self.saldo}')
@@ -18,4 +43,3 @@ class cliente:
         self.saldo = self.saldo + valor_deposito
 
     
-
--- testedosistema.py
+++ testedosistema.py
@@ -1,8 +1,9 @@
 from sistema import *
 
-cliente1 = cliente('Vinicius','3211.013.4210-0',1000.00)
+cliente.cadastra_cliente(cliente)
+cliente.lista_cliente(cliente)
 
-print(f'O cliente {cliente1.nome} tem a conta {cliente1.conta} com o saldo de R$ {cliente1.saldo}')
-cliente1.saque()
-cliente1.deposito()
-print(f'O cliente {cliente1.nome} tem a conta {cliente1.conta} com o saldo de R$ {cliente1.saldo}')
+acao_cliente.consulta_saldo(cliente)
+acao_cliente.deposito(cliente)
+acao_cliente.saque(cliente)
+acao_cliente.consulta_saldo(cliente)
